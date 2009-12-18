997b625e1bf06a7ceea833adf0130dca81cadbdb
Tobias Koch <tobias.koch@gmail.com>
2009-12-18 23:43:29 +0100

    - Improvements to debian/package.sh

--- debian/package.sh
+++ debian/package.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
 
 function usage {
-	echo "Usage: debian/package.sh <tag>"
+	echo "Usage: debian/package.sh <tag> <outdir>"
 }
 
-if [ $# -lt 1 ] || [ -z "`echo $0 | egrep '^debian/'`" ]; then
+if [ $# -ne 2 ] || [ -z "`echo $0 | egrep '^debian/'`" ]; then
 	usage
 	exit 1
 fi
 
 TAG=$1
+OUTDIR=$2
 OLD_BRANCH=`git branch | egrep "^\*" | cut -d' ' -f2`
+UPSTREAM="`echo $TAG | sed 's/\(^.*\)-[[:digit:]]$/\1/'`"
+TMPDIR="`mktemp -d`"
+
+test -d ${OUTDIR} || {
+	echo "No such directory '$OUTDIR'."
+	exit 2
+}
+
+OUTDIR=$(cd ${OUTDIR} && pwd)
 
 git checkout $TAG > /dev/null 2>&1 || {
 	echo "Could not check out tag $TAG."
-	exit 2
+	exit 3
 }
 
 git checkout $OLD_BRANCH > /dev/null 2>&1
 
-git archive --prefix=ecromedos-${TAG}/ --format=tar ${TAG} | \
-	(cd .. && \
-	tar -x --exclude=.gitignore --exclude=debian -f - && \
-	tar -czf ecromedos-${TAG}.orig.tar.gz ecromedos-${TAG} \
-	&& rm -fr ecromedos-${TAG})
+echo "Building package in $TMPDIR:"
 
-git archive --prefix=ecromedos-${TAG}/ --format=tar pkg | \
-	(cd .. && tar -x --exclude=.gitignore -f -)
+git archive --prefix=ecromedos-${UPSTREAM}/ --format=tar ${TAG} | \
+	(cd ${TMPDIR} && \
+	tar -x --exclude=.gitignore --exclude=debian -f - && \
+	tar -czf ecromedos_${UPSTREAM}.orig.tar.gz ecromedos-${UPSTREAM} \
+	&& rm -fr ecromedos-${UPSTREAM})
+
+git archive --prefix=ecromedos-${UPSTREAM}/ --format=tar ${TAG} | \
+	(cd ${TMPDIR} && tar -x --exclude=.gitignore --exclude=debian/package.sh -f -)
+
+(cd ${TMPDIR}/ecromedos-${UPSTREAM} &&
+	dpkg-buildpackage -us -uc &&
+	cd .. &&
+	mv *.{changes,deb,dsc,diff.gz,tar.gz} ${OUTDIR})
+
+rm -fr ${TMPDIR}
+
+echo "Collect package from $OUTDIR."
 
