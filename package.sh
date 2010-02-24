32fa074fcd3836db2f0f5a574a874e501ca04aec
Tobias Koch <tobias.koch@gmail.com>
2010-02-24 16:54:02 +0200

    - Step 1 in cleaning up the packaging script

--- package.sh
+++ package.sh
@@ -1,48 +1,113 @@
 #!/bin/bash
 
-function usage {
+###############################################################################
+
+ERR_USAGE=1
+ERR_NOTAG=2
+ERR_NODIR=3
+
+###############################################################################
+
+function usage #()
+{
 	echo "Usage: package.sh <tag> <outdir>"
 }
 
-if [ $# -ne 2 ] || [ ! -f ecromedos.spec ]; then
+function error #(code, msg)
+{
+	local code=$1
+	local msg=$2
+	echo "$msg" >&2
+	exit $code
+}
+
+function check_tag #(tag)
+{
+	local tag=$1
+	local current_branch=`git branch | egrep "^\*" | cut -d' ' -f2`
+
+	# check for tag
+	git checkout $tag > /dev/null 2>&1 || {
+		error $ERR_NOTAG "Could not check out tag '$tag'."
+	}
+
+	if [ -n "$current_branch" ]; then
+		git checkout $current_branch > /dev/null 2>&1
+	else
+		git checkout $master > /dev/null 2>&1
+	#end if
+}
+
+function check_outdir #(outdir)
+{
+	local outdir=$1
+
+	# check for existence
+	test -d "$outdir" || {
+		echo "No such directory '$outdir'."
+		exit $ERR_NODIR
+	}
+
+	# return abs path
+	echo $(cd ${outdir} && pwd)
+}
+
+function build_rpm #(tag, outdir)
+{
+	local tag=$1
+	local outdir=$2
+	local tmpdir="`mktemp -d`"
+
+	echo -n "Building RPM package in $tmpdir..."
+
+	# create rpm build subdirs
+	mkdir -p $tmpdir/{RPMS,BUILD,SOURCES,SPECS,SRPMS}
+
+	# build tarball
+	git archive --prefix=ecromedos-${tag}/ --format=tar ${tag} | \
+		(cd ${tmpdir}/SOURCES && \
+		tar -x \
+			--exclude=.gitignore \
+			--exclude=debian \
+			--exclude=ecromedos.spec \
+			--exclude=package.sh -f - && \
+		tar -czf ecromedos-${tag}.tar.gz ecromedos-${tag} && \
+		rm -fr ecromedos-${tag})
+
+	# update tag in spec file
+	cat ecromedos.spec | sed "s/###TAG###/$tag/" > \
+		$tmpdir/SPECS/ecromedos-${tag}.spec
+
+	# build package
+	rpmbuild --nodeps --target="noarch" --define="%_topdir $tmpdir" \
+		-ba $tmpdir/SPECS/ecromedos-${tag}.spec
+
+	# fetch build artifacts
+	mv $tmpdir/RPMS/*/*.rpm $outdir
+	mv $tmpdir/SRPMS/*.rpm $outdir
+
+	# cleanup
+	rm -fr $tmpdir
+
+	echo "done"
+}
+
+###############################################################################
+
+if [ $# -ne 2 ]; then
 	usage
-	exit 1
+	exit $ERR_USAGE
 fi
 
 TAG=$1
 OUTDIR=$2
-OLD_BRANCH=`git branch | egrep "^\*" | cut -d' ' -f2`
-TMPDIR="`mktemp -d`"
 
-test -d ${OUTDIR} || {
-	echo "No such directory '$OUTDIR'."
-	exit 2
-}
+# SANITY CHECKS
+check_outdir $OUTDIR
+check_tag $TAG
 
-OUTDIR=$(cd ${OUTDIR} && pwd)
+# BUILD PACKAGES
+build_rpm $TAG $OUTDIR
 
-git checkout $TAG > /dev/null 2>&1 || {
-	echo "Could not check out tag $TAG."
-	exit 3
-}
-
-git checkout $OLD_BRANCH > /dev/null 2>&1
-
-echo "Building package in $TMPDIR:"
-
-mkdir -p $TMPDIR/{RPMS,BUILD,SOURCES,SPECS,SRPMS}
-
-git archive --prefix=ecromedos-${TAG}/ --format=tar ${TAG} | \
-	(cd ${TMPDIR}/SOURCES && \
-	tar -x --exclude=.gitignore --exclude=debian --exclude=ecromedos.spec --exclude=package.sh -f - && \
-	tar -czf ecromedos-${TAG}.tar.gz ecromedos-${TAG} \
-	&& rm -fr ecromedos-${TAG})
-
-cat ecromedos.spec | sed "s/###TAG###/$TAG/" > $TMPDIR/SPECS/ecromedos-${TAG}.spec
-rpmbuild --nodeps --target="noarch" --define="%_topdir $TMPDIR" -ba $TMPDIR/SPECS/ecromedos-${TAG}.spec
-mv $TMPDIR/RPMS/*/*.rpm $OUTDIR
-mv $TMPDIR/SRPMS/*.rpm $OUTDIR
-rm -fr $TMPDIR
-
-echo "Collect package from $OUTDIR."
+echo "Collect artifacts from $OUTDIR."
 
