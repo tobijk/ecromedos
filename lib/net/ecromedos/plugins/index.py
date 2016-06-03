c1ab2a3080170ed02e9f186718f8667cb2b42d73
Tobias Koch <tobias@tobijk.de>
2016-06-04 00:58:36 +0300

    Port 'index' plugin to lxml

--- lib/net/ecromedos/plugins/index.py
+++ lib/net/ecromedos/plugins/index.py
@@ -6,7 +6,8 @@
 # URL:     http://www.ecromedos.net
 #
 
-import lxml, sys, locale
+import sys, locale, functools
+import lxml.etree as etree
 from net.ecromedos.error import ECMDSPluginError
 
 def getInstance(config):
@@ -17,65 +18,12 @@ def getInstance(config):
 class Plugin():
 
     def __init__(self, config):
-        self.__index = {}
-        self.__counter = 0
+        self.index = {}
+        self.counter = 0
         try:
             self.__draft = config['xsl_params']['global.draft']
         except KeyError:
             self.__draft = "'no'"
-    #end function
-
-    def __saveNode(self, node):
-        """Substitutes a 'defterm' node with a label and stores a reference
-        to the node for later when the index is to be built."""
-
-        sortkey = node.prop("sortkey")
-        group = node.prop("group")
-        if not group: group = "default"
-
-        item = None
-        subitem = None
-        subsubitem = None
-
-        # read idxterm items
-        child = node.children
-        while child:
-            if child.type == "element":
-                if child.name == "item":
-                    item = child.getContent().strip()
-                elif child.name == "subitem":
-                    subitem = child.getContent().strip()
-                elif child.name == "subsubitem":
-                    subsubitem = child.getContent().strip()
-                #end if
-            #end if
-            child = child.__next__
-        #end for
-
-        # create new 'label' node
-        label_node = libxml2.newNode("label")
-        label_id   = "idx:item%06d" % self.__counter
-        label_node.setProp("id", label_id)
-
-        # substitute the label for the idxterm
-        node.replaceNode(label_node)
-        node.freeNode()
-
-        # at least 'item' must exist
-        if item != None:
-            index = self.__index.setdefault(group, [group, {}, [], None])
-            for x in [item, subitem, subsubitem, None]:
-                if not x:
-                    index[2].append(label_id)
-                    index[3] = sortkey
-                    break
-                #end if
-                index = index[1].setdefault(x, [x, {}, [], None])
-            #end for
-        #end if
-
-        self.__counter += 1
-        return label_node
     #end function
 
     def process(self, node, format):
@@ -83,14 +31,94 @@ class Plugin():
         depending on which node triggered the plugin."""
 
         # skip if in draft mode
-        if self.__draft == "'yes'": return node
+        if self.__draft == "'yes'":
+            return node
 
-        if node.name == "idxterm":
+        if node.tag == "idxterm":
             node = self.__saveNode(node)
-        elif node.name == "make-index":
+        elif node.tag == "make-index":
             node = self.__makeIndex(node)
         #end if
+
         return node
+    #end function
+
+    def flush(self):
+        self.index = {}
+        self.counter = 0
+    #end function
+
+    # PRIVATE
+
+    def __saveNode(self, node):
+        """Substitutes a 'defterm' node with a label and stores a reference
+        to the node for later when the index is to be built."""
+
+        sortkey = node.attrib.get("sortkey", None)
+        group   = node.attrib.get("group", "default")
+
+        item = None
+        subitem = None
+        subsubitem = None
+
+        # read idxterm items
+        for child in node.iterchildren():
+            if child.tag == "item":
+                item = child.text.strip()
+            elif child.tag == "subitem":
+                subitem = child.text.strip()
+            elif child.tag == "subsubitem":
+                subsubitem = child.text.strip()
+            #end if
+        #end for
+
+        # create label
+        label_id = "idx:item%06d" % self.counter
+
+        # replace node itself with label node
+        label_node = etree.Element("label", id=label_id)
+        label_node.tail = node.tail
+        node.getparent().replace(node, label_node)
+
+        # at least 'item' must exist
+        if item != None:
+            index = self.index.setdefault(group, [group, {}, [], None])
+
+            for entry in [item, subitem, subsubitem, None]:
+                if entry is None:
+                    index[2].append(label_id)
+                    index[3] = sortkey
+                    break
+                #end if
+
+                index = index[1].setdefault(entry, [entry, {}, [], None])
+            #end for
+        #end if
+
+        self.counter += 1
+        return label_node
+    #end function
+
+    def __makeIndex(self, node):
+        """Read configuration. Sort items. Build index. Build XML."""
+
+        if not self.index:
+            return node
+
+        # build configuration
+        config = self.__configuration(node)
+
+        # set locale
+        self.__setLocale(config['locale'], config['locale_encoding'],
+                config['locale_variant'])
+
+        # build DOM structures
+        index = self.__buildIndex(node, config)
+
+        # reset locale
+        self.__resetLocale()
+
+        return index
     #end function
 
     def __configuration(self, node):
@@ -99,21 +127,17 @@ class Plugin():
 
         # presets
         properties = {
-            "alphabet" : "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z",
-            "locale"   : "C",
+            "columns": "2",
+            "group": "default",
+            "separator": ", ",
+            "locale": "C",
             "locale_encoding": None,
             "locale_variant": None,
-            "columns"  : "2",
-            "group"    : "default",
-            "separator": ", "
+            "alphabet": "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"
         }
 
         # read element attributes
-        prop = node.properties
-        while prop:
-            properties[prop.name] = prop.content
-            prop = prop.__next__
-        #end while
+        properties.update(dict(node.items()))
 
         # split locale into locale/encoding/variant
         if '@' in properties['locale']:
@@ -124,9 +148,9 @@ class Plugin():
                 properties['locale'].split('.', 1)
         #end ifs
 
-        # build list of alphabet characters
+        # parse the alphabet
         alphabet = []
-        for ch in [x.strip() for x in properties['alphabet'].split()]:
+        for ch in [x.strip() for x in properties['alphabet'].split(",")]:
             if ch[0] == '[' and ch[-1] == ']':
                 properties['symbols'] = ch[1:-1].strip()
             else:
@@ -143,23 +167,25 @@ class Plugin():
         variant."""
 
         success = False
-        for e in [encoding, "UTF8"]:
-            if success: break
+
+        for e in [encoding, "UTF-8"]:
+            if success:
+                break
             for v in [variant, ""]:
-                localestring = '.'.join([x for x in [collate, e] if x])
+                localestring = '.'.join([x for x in [collate,      e] if x])
                 localestring = '@'.join([x for x in [localestring, v] if x])
                 try:
                     locale.setlocale(locale.LC_COLLATE, localestring)
                     success = True
                     break
-                except locale.Error: pass
+                except locale.Error:
+                    pass
             #end for
         #end for
 
         if not success:
             msg = "Warning: cannot set locale '%s'." % collate
             sys.stderr.write(msg)
-        #end if
     #end function
 
     def __resetLocale(self):
@@ -167,67 +193,53 @@ class Plugin():
         locale.resetlocale(locale.LC_COLLATE)
     #end function
 
-    def __adaptEncoding(self, itemlist, encoding):
-        """Adapt all sortkeys' encoding to the locale's encoding."""
-
-        if encoding and encoding.replace('-','') != "UTF8":
-            i = 0
-            while i < len(itemlist):
-                values = itemlist[i]
-                values[-1] = values[-1].decode("UTF-8").encode(encoding)
-                i += 1
-            #end for
-        #end if
-    #end function
-
-    def __sortIndex(self, index, level="item", config=None, encoding=None):
+    def __sortIndex(self, index, level="item", config=None):
         """Sort index terms."""
 
         # stop recursion
-        if not index: return index
+        if not index:
+            return index
 
         # recursive sortkey evaluation
         itemlist = []
         for v in index.values():
             # set sortkey
-            if not v[-1]: v[-1] = v[0]
+            if not v[-1]:
+                v[-1] = v[0]
             # recursion
-            v[1]  = self.__sortIndex(v[1], "sub"+level, config, encoding)
+            v[1] = self.__sortIndex(v[1], "sub"+level, config)
             itemlist.append(v)
         #end for
 
         # insert alphabet
         if level == "item":
             for ch in config['alphabet']:
-                newnode = libxml2.newNode("idxsection")
-                newnode.setProp("name", ch)
+                newnode = etree.Element("idxsection", name=ch)
                 itemlist.append(["idxsection", newnode, ch])
             #end for
         #end if
 
-        # adapt strings' encoding to locale
-        self.__adaptEncoding(itemlist, encoding)
-
         # comparison function
         def compare(a,b):
             result = locale.strcoll(a[-1], b[-1])
+
             y1 = a[1]
             y2 = b[1]
+
             if result != 0:
                 return result
-            elif isinstance(y1, libxml2.xmlNode) \
-                    and isinstance(y2, libxml2.xmlNode):
+            elif isinstance(y1, etree._Element) and \
+                    isinstance(y2, etree._Element):
                 return 0
-            elif isinstance(y1, libxml2.xmlNode):
+            elif isinstance(y1, etree._Element):
                 return -1
-            elif isinstance(y2, libxml2.xmlNode):
+            elif isinstance(y2, etree._Element):
                 return +1
             else:
                 return 0
         #end inline
 
-        # sort index
-        itemlist.sort(compare)
+        itemlist.sort(key=functools.cmp_to_key(compare))
         return itemlist
     #end function
 
@@ -235,31 +247,44 @@ class Plugin():
         """Build index recursively from nested lists structure."""
 
         # stop recursion
-        if not index: return index
+        if not index:
+            return index
 
         for item in index:
             term = item[0]
-            item_node = libxml2.newNode(level)
-            item_node.addChild(libxml2.newText(term))
-            # build referrer node
+
+            item_node = etree.Element(level)
+            item_node.text = term
+
             i = 0
             references = item[2]
             num_ref = len(references)
+
+            # build referrer node
             while i < num_ref:
                 ref = references[i]
+
                 # add single space
                 if i == 0:
-                    item_node.addChild(libxml2.newText(" "))
+                    item_node.text += " "
+
                 # add reference to list
-                idxref_node = libxml2.newNode("idxref")
-                idxref_node.setProp("idref", ref)
-                item_node.addChild(idxref_node)
+                etree.SubElement(item_node, "idxref", idref=ref)
+
                 # add separator (this should be configurable)
                 if i < num_ref - 1:
-                    item_node.addChild(libxml2.newText(separator))
+                    if not item_node[-1].tail:
+                        item_node[-1].tail = separator
+                    else:
+                        item_node[-1].tail += separator
+                    #end if
+                #end if
+
                 i += 1
             #end while
-            section.addChild(item_node)
+
+            section.append(item_node)
+
             # recursion
             self.__buildIndexHelper(section, item[1], "sub"+level, separator)
         #end for
@@ -269,99 +294,82 @@ class Plugin():
         """Build XML DOM structure."""
 
         # detect group name
-        group = node.prop("group")
-        if not group: group = "default"
+        group = node.attrib.get("group", "default")
 
         # load group
         try:
-            index = self.__index[group][1]
+            index = self.index[group][1]
         except:
             return node
         #end try
 
         # sort index
         localestring, encoding = locale.getlocale(locale.LC_COLLATE)
-        index = self.__sortIndex(index, level="item",
-            config=config, encoding=encoding)
+        index = self.__sortIndex(index, level="item", config=config)
 
         # build base node
-        index_node = libxml2.newNode("index")
+        index_node = etree.Element("index")
         for prop_name in ["columns", "title", "tocentry"]:
             try:
-                index_node.setProp(prop_name, config[prop_name])
+                index_node.attrib[prop_name] = config[prop_name]
             except KeyError: pass
         #end for        
 
         # start building index...
-        section = libxml2.newNode("idxsection")
+        section = etree.Element("idxsection")
         try:
-            section.setProp("name", config['symbols'])
+            section.attrib["name"] = config['symbols']
         except KeyError: pass
 
         separator = config["separator"]
 
         for item in index:
-            if isinstance(item[1], libxml2.xmlNode):
-                index_node.addChild(section)
+            if isinstance(item[1], etree._Element):
+                index_node.append(section)
                 section = item[1]
             else:
                 term = item[0]
-                item_node = libxml2.newNode("item")
-                item_node.addChild(libxml2.newText(term))
-                # build referrer node
+                item_node = etree.Element("item")
+                item_node.text = term
+
                 i = 0
                 references = item[2]
                 num_ref = len(references)
+
+                # build referrer node
                 while i < num_ref:
                     ref = references[i]
+
                     # add single space
                     if i == 0:
-                        item_node.addChild(libxml2.newText(" "))
+                        item_node.text += " "
+
                     # add reference to list
-                    idxref_node = libxml2.newNode("idxref")
-                    idxref_node.setProp("idref", ref)
-                    item_node.addChild(idxref_node)
+                    etree.SubElement(item_node, "idxref", idref=ref)
+
                     # add separator (this should be configurable)
                     if i < num_ref - 1:
-                        item_node.addChild(libxml2.newText(separator))
+                        if not item_node[-1].tail:
+                            item_node[-1].tail = separator
+                        else:
+                            item_node[-1].tail += separator
+                        #end if
+                    #end if
+
                     i += 1
                 #end while
-                section.addChild(item_node)
+
+                section.append(item_node)
+
                 # recursion
                 self.__buildIndexHelper(section, item[1], "subitem", separator)
             #end if
         #end for
-        index_node.addChild(section)
 
-        # clean up and return new node
-        node.replaceNode(index_node)
-        node.freeNode()
-
+        index_node.append(section)
+        index_node.tail = node.tail
+        node.getparent().replace(node, index_node)
         return index_node
-    #end function
-
-    def __makeIndex(self, node):
-        """Read configuration. Sort items. Build index. Build XML."""
-
-        # read node attributes and build configuration
-        config = self.__configuration(node)
-
-        # set locale
-        self.__setLocale(config['locale'],
-            config['locale_encoding'], config['locale_variant'])
-
-        # build DOM structures
-        index = self.__buildIndex(node, config)
-
-        # reset locale
-        self.__resetLocale()
-
-        return index
-    #end function
-
-    def flush(self):
-        self.__index = {}
-        self.__counter = 0
     #end function
 
 #end class
