084340d5ab58267f691626d84f4ed6c8059a4678
Tobias Koch <tobias@tobijk.de>
2016-06-04 19:05:06 +0300

    Highlight plugin whitespace

--- test/ut/test_plugin_highlight.py
+++ test/ut/test_plugin_highlight.py
@@ -17,7 +17,7 @@ import net.ecromedos.plugins.highlight as highlight
 class UTTestPluginHighlight(unittest.TestCase):
 
     def test_highlightCCode(self):
-        expected_result = b'<root>\n    <code bgcolor="#f8f8f8" syntax="c" startline="13" linestep="3" colorscheme="default"><b>0013</b> <color rgb="#bc7a00">#</color><color rgb="#bc7a00">include &lt;stdlib.h&gt;</color><color rgb="#bc7a00">\n</color><b>0016</b> <color rgb="#bc7a00">#</color><color rgb="#bc7a00">include &lt;stdio.h&gt;</color><color rgb="#bc7a00">\n</color><b>0019</b> \n<b>0022</b> <color rgb="#b00040">int</color> <color rgb="#0000ff">main</color>(<color rgb="#b00040">int</color> argc, <color rgb="#b00040">char</color> <color rgb="#666666">*</color>argv[])\n<b>0025</b> {\n<b>0028</b>     printf(<color rgb="#ba2121">"</color><color rgb="#ba2121">Hello World!</color>\n<b>0031</b> <color rgb="#ba2121">"</color><color rgb="#ba2121">);</color>\n<b>0034</b>     <color rgb="#008000"><b>return</b></color> <color rgb="#666666">0</color>;\n<b>0037</b> }\n<b>0040</b>     \n</code></root>'
+        expected_result = b'<root>\n    <code bgcolor="#f8f8f8" syntax="c" startline="13" linestep="3" colorscheme="default"><b>0013</b> <color rgb="#bc7a00">#</color><color rgb="#bc7a00">include &lt;stdlib.h&gt;</color><color rgb="#bc7a00">\n</color><b>0016</b> <color rgb="#bc7a00">#</color><color rgb="#bc7a00">include &lt;stdio.h&gt;</color><color rgb="#bc7a00">\n</color><b>0019</b> \n<b>0022</b> <color rgb="#b00040">int</color> <color rgb="#0000ff">main</color>(<color rgb="#b00040">int</color> argc, <color rgb="#b00040">char</color> <color rgb="#666666">*</color>argv[])\n<b>0025</b> {\n<b>0028</b>     printf(<color rgb="#ba2121">"</color><color rgb="#ba2121">Hello World!</color>\n<b>0031</b> <color rgb="#ba2121">"</color><color rgb="#ba2121">);</color>\n<b>0034</b>     <color rgb="#008000"><b>return</b></color> <color rgb="#666666">0</color>;\n<b>0037</b> }\n<b>0040</b>     \n</code>\n</root>'
 
         code = """
 <root>
@@ -41,6 +41,7 @@ int main(int argc, char *argv[])
         plugin.process(root.find("./code"), "xhtml")
 
         result = etree.tostring(tree, encoding="utf-8", method="xml")
+
         self.assertEqual(result, expected_result)
     #end function
 
