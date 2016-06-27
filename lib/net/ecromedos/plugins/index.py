7ce854eab042e118665b291bc6136d194ccb538c
Tobias Koch <tobias@tobijk.de>
2016-06-27 22:18:49 +0300

    Fix sorting bug in index plugin

--- lib/net/ecromedos/plugins/index.py
+++ lib/net/ecromedos/plugins/index.py
@@ -221,10 +221,17 @@ class Plugin():
 
         # comparison function
         def compare(a,b):
-            result = locale.strcoll(a[-1], b[-1])
+            x1 = a[-1]
+            x2 = b[-1]
 
             y1 = a[1]
             y2 = b[1]
+
+            if isinstance(y1, etree._Element) != isinstance(y2, etree._Element):
+                result = locale.strcoll(x1.lower(), x2.lower())
+            else:
+                result = locale.strcoll(a[-1], b[-1])
+            #end if
 
             if result != 0:
                 return result
