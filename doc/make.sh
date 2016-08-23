6f010939213182c5b2770387a9830ece35385667
Tobias Koch <tobias@tobijk.de>
2016-08-24 00:08:29 +0300

    Import user manual from external repo

---
+++ doc/make.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+do_clean() {
+    rm -fr html pdf spool
+}    
+ 
+do_build() {
+    mkdir -p html pdf spool
+
+    (cd html  && ../../bin/ecromedos -f xhtml   ../src/manual.xml)
+    (cd spool && ../../bin/ecromedos -f xelatex ../src/manual.xml)
+
+    (
+        cd spool
+        for i in `seq 1 3`
+        do
+            xelatex main.tex
+        done
+    )
+
+    cp spool/main.pdf  pdf/user-manual.pdf
+    cp spool/main.pdf html/user-manual.pdf
+}
+
+case "$1" in
+    clean)
+        do_clean
+        ;;
+    *)
+        do_clean
+        do_build
+        ;;
+esac
+
